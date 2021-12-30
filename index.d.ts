ea337d54b58715098dad85b959f1fa1e834197ab
Mikl Wolfe <wolfemikl@gmail.com>
2021-12-29 22:38:06 -0700

    Include global types

---
+++ index.d.ts
@@ -0,0 +1,13 @@
+import { Listing, Submission } from 'snoowrap';
+
+declare global {
+  type RedditResponseType = Listing<Submission> | never[];
+  type ContentType = (Element | boolean)[];
+  type AppendType = (
+    content: ContentType,
+    responseInput: RedditResponseType,
+    windowOwner: string[],
+    app: Element,
+    willContinue: boolean,
+  ) => void;
+}
