e3644b4e839295c90e377d199ef58aaeaf172be6
Mikl Wolfe <wolfemikl@gmail.com>
2022-01-01 20:29:49 -0700

    Add switch case for each filter type

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -7,6 +7,9 @@ import {
   getNewSubmissions,
   getUserSubmissions,
   getTopSubmissions,
+  getHotSubmissions,
+  getRisingSubmissions,
+  getControversialSubmissions,
 } from '../requests/getSubmissions';
 
 const app = document.querySelector('.app');
@@ -19,17 +22,45 @@ let response: RedditResponseType;
 
 const App = async () => {
   const windowOwner = window.location.pathname.split('/').slice(1);
-  if (windowOwner[0] === 'u') {
-    response = await getUserSubmissions(windowOwner[1]);
-  } else if (windowOwner[0] === 'r') {
-    if (windowOwner.length > 2) {
-      response = await getTopSubmissions(
-        windowOwner[1],
-        windowOwner[2] as Timespan,
-      );
-    } else {
-      response = await getNewSubmissions(windowOwner[1]);
-      willContinue = true;
+  const subUser = windowOwner[0];
+  const subName = windowOwner[1];
+  const filterType = windowOwner[2];
+  const timespan = windowOwner[3];
+
+  if (subUser === 'u') {
+    response = await getUserSubmissions(subName);
+  } else if (subUser === 'r') {
+    switch (filterType) {
+      case 'hot': {
+        response = await getHotSubmissions(subName);
+
+        break;
+      }
+      case 'rising': {
+        response = await getRisingSubmissions(subName);
+
+        break;
+      }
+      case 'controversial': {
+        response = await getControversialSubmissions(subName);
+
+        break;
+      }
+      case 'new': {
+        response = await getNewSubmissions(subName);
+
+        break;
+      }
+      case 'top': {
+        const time = timespan || 'all';
+        response = await getTopSubmissions(subName, time as Timespan);
+
+        break;
+      }
+      default: {
+        response = await getNewSubmissions(subName);
+        willContinue = true;
+      }
     }
   }
   const content = filter(response);
