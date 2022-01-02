174f507326e2f2b0ac9b34802c1f4a38f49bd29e
Mikl Wolfe <wolfemikl@gmail.com>
2022-01-01 23:11:05 -0700

    Include oauth condition

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -2,12 +2,19 @@ import Snoowrap from 'snoowrap';
 
 import token from '../../../token';
 
-const r = new Snoowrap({
-  userAgent: token.userAgent,
-  clientId: token.clientId,
-  clientSecret: token.clientSecret,
-  username: token.username,
-  password: token.password,
-});
+const r = token
+  ? new Snoowrap({
+      userAgent: token.userAgent,
+      clientId: token.clientId,
+      clientSecret: token.clientSecret,
+      username: token.username,
+      password: token.password,
+    })
+  : new Snoowrap({
+      userAgent: 'put your user-agent string here',
+      clientId: 'put your client id here',
+      clientSecret: 'put your client secret here',
+      refreshToken: 'put your refresh token here',
+    });
 
 export default r;
