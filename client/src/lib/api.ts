3abb0e5569152053ac5c5f2242fa45fb6969a1c7
Mikl Wolfe <wolfemikl@gmail.com>
2022-01-05 23:16:21 -0700

    Include test

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -4,9 +4,11 @@ import auth from './auth';
 
 const token = await auth();
 
+console.log(token);
 const r = new Snoowrap({
   userAgent: 'View-Master 3000',
   accessToken: token,
 });
+console.log(r);
 
 export default r;
