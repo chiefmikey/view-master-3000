9c1430d9fbc4cf1575e439b8aee77a2c42626504
Mikl Wolfe <wolfemikl@gmail.com>
2022-01-05 22:01:50 -0700

    Update token variable

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -2,9 +2,11 @@ import Snoowrap from 'snoowrap';
 
 import auth from './auth';
 
+const token = await auth();
+
 const r = new Snoowrap({
   userAgent: 'View-Master 3000',
-  accessToken: await auth(),
+  accessToken: token,
 });
 
 export default r;
