2727ca79db9d508953084a546048f87116f4672c
Mikl Wolfe <wolfemikl@gmail.com>
2022-01-05 21:30:27 -0700

    Update api credentials

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,23 +1,8 @@
 import Snoowrap from 'snoowrap';
 
-import token from '../../../token';
-
-// if (process.env.NODE_ENV !== 'production') {
-//   await import('../../../token');
-// }
-
-const r =
-  process.env.NODE_ENV !== 'production'
-    ? await new Snoowrap({
-        userAgent: token.userAgent || '',
-        clientId: token.clientId,
-        clientSecret: token.clientSecret,
-        username: token.username,
-        password: token.password,
-      })
-    : new Snoowrap({
-        userAgent: 'View-Master 3000',
-        accessToken: '',
-      });
+const r = new Snoowrap({
+  userAgent: 'View-Master 3000',
+  accessToken: '',
+});
 
 export default r;
