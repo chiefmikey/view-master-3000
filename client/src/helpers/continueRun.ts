d6f0f75c3a336951f7053e7756bd336e95864fdf
Mikl Wolfe <wolfemikl@gmail.com>
2022-01-12 18:11:48 -0700

    Update continue run conditions

--- client/src/helpers/continueRun.ts
+++ client/src/helpers/continueRun.ts
@@ -7,24 +7,29 @@ const tops = ['hour', 'day', 'week', 'month', 'year', 'all'];
 let topIndex = 0;
 
 const continueRun = async (windowOwner: string[]) => {
-  if (responseIndex === responseFunctions.length) {
-    response = await axios.get('https://api.5105015032.com/auth/vm3000/top', {
-      params: { subName: windowOwner[1], time: tops[topIndex] },
-    });
-    if (topIndex === tops.length - 1) {
-    } else {
+  try {
+    if (!responseFunctions[responseIndex] && tops[topIndex]) {
+      response = await axios.get('https://api.5105015032.com/auth/vm3000/top', {
+        params: { subName: windowOwner[1], time: tops[topIndex] },
+      });
       topIndex += 1;
+    } else if (responseFunctions[responseIndex]) {
+      response = await axios.get(
+        `https://api.5105015032.com/auth/vm3000/${responseFunctions[responseIndex]}`,
+        {
+          params: { subName: windowOwner[1] },
+        },
+      );
+      responseIndex += 1;
     }
-  } else {
-    response = await axios.get(
-      `https://api.5105015032.com/auth/vm3000/${responseFunctions[responseIndex]}`,
-      {
-        params: { subName: windowOwner[1] },
-      },
-    );
-    responseIndex += 1;
+    if (response.data) {
+      return response.data;
+    }
+    return [];
+  } catch (error) {
+    console.log(error);
+    return [];
   }
-  return response.data;
 };
 
 export default continueRun;
