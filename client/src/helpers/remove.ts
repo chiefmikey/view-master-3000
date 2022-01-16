43c80f0ba7359a0aac83fa78303076b746336a73
chiefmikey <wolfemikl@gmail.com>
2022-01-16 10:55:59 -0600

    Include iteration condition for undefined elements

--- client/src/helpers/remove.ts
+++ client/src/helpers/remove.ts
@@ -1,13 +1,16 @@
-const remove = (app: Element): void => {
+const remove = (app: Element) => {
   const children = [];
   for (let index = 0; index < 4; index += 1) {
-    children.push(app.children[index]);
+    if (app.children[index]) {
+      children.push(app.children[index]);
+    }
   }
   for (const child of children) {
     if (child) {
       child.remove();
     }
   }
+  return children;
 };
 
 export default remove;
