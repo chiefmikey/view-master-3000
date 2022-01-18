d9e6fc1c2e3d7e1fef49849121dd66a0771f9613
Mikl Wolfe <wolfemikl@gmail.com>
2022-01-18 05:36:42 -0700

    Include response length variable to avoid splicing loop bug

--- client/src/helpers/restore.ts
+++ client/src/helpers/restore.ts
@@ -1,13 +1,14 @@
 const restore = (app: Element | undefined, previousResponses: Element[]) => {
   const children = [];
   const responses = previousResponses;
+  const responseLength = responses.length;
   if (responses) {
     for (
-      let index = responses.length - 1;
-      index >= responses.length - 4;
+      let index = responseLength - 1;
+      index >= responseLength - 4;
       index -= 1
     ) {
-      console.log(index, responses.length);
+      console.log(index, responseLength);
       if (responses[index]) {
         children.push(responses[index]);
         responses.splice(index, 1);
