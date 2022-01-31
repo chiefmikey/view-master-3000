04a1a7919d22820f1e944b12678d0aee3f72a6c2
Mikl Wolfe <wolfemikl@gmail.com>
2022-01-31 08:44:02 -0700

    Include search api call method

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -26,83 +26,102 @@ const App = async () => {
     let filterTag = '';
     let tagType: string[];
 
-    if (subUser === 'u') {
-      filterTag = windowOwner[2];
-      response = await axios.get(
-        'https://api.5105015032.com/auth/vm3000/user',
-        {
-          params: { subName },
-        },
-      );
-    } else if (subUser === 'r') {
-      switch (filterType) {
-        case 'hot': {
-          filterTag = windowOwner[3];
-          response = await axios.get(
-            'https://api.5105015032.com/auth/vm3000/hot',
-            {
-              params: { subName },
-            },
-          );
+    switch (subUser) {
+      case 'u': {
+        filterTag = windowOwner[2];
+        response = await axios.get(
+          'https://api.5105015032.com/auth/vm3000/user',
+          {
+            params: { subName },
+          },
+        );
 
-          break;
-        }
-        case 'rising': {
-          filterTag = windowOwner[3];
-          response = await axios.get(
-            'https://api.5105015032.com/auth/vm3000/rising',
-            {
-              params: { subName },
-            },
-          );
-
-          break;
-        }
-        case 'controversial': {
-          filterTag = windowOwner[3];
-          response = await axios.get(
-            'https://api.5105015032.com/auth/vm3000/controversial',
-            {
-              params: { subName },
-            },
-          );
-
-          break;
-        }
-        case 'new': {
-          filterTag = windowOwner[3];
-          response = await axios.get(
-            'https://api.5105015032.com/auth/vm3000/new',
-            {
-              params: { subName },
-            },
-          );
-
-          break;
-        }
-        case 'top': {
-          filterTag = windowOwner[4];
-          const time = timespan || 'all';
-          response = await axios.get(
-            'https://api.5105015032.com/auth/vm3000/top',
-            {
-              params: { subName, time },
-            },
-          );
-
-          break;
-        }
-        default: {
-          filterTag = windowOwner[2];
-          response = await axios.get(
-            'https://api.5105015032.com/auth/vm3000/new',
-            {
-              params: { subName },
-            },
-          );
-          willContinue = true;
-        }
+        break;
       }
+      case 'r': {
+        switch (filterType) {
+          case 'hot': {
+            filterTag = windowOwner[3];
+            response = await axios.get(
+              'https://api.5105015032.com/auth/vm3000/hot',
+              {
+                params: { subName },
+              },
+            );
+
+            break;
+          }
+          case 'rising': {
+            filterTag = windowOwner[3];
+            response = await axios.get(
+              'https://api.5105015032.com/auth/vm3000/rising',
+              {
+                params: { subName },
+              },
+            );
+
+            break;
+          }
+          case 'controversial': {
+            filterTag = windowOwner[3];
+            response = await axios.get(
+              'https://api.5105015032.com/auth/vm3000/controversial',
+              {
+                params: { subName },
+              },
+            );
+
+            break;
+          }
+          case 'new': {
+            filterTag = windowOwner[3];
+            response = await axios.get(
+              'https://api.5105015032.com/auth/vm3000/new',
+              {
+                params: { subName },
+              },
+            );
+
+            break;
+          }
+          case 'top': {
+            filterTag = windowOwner[4];
+            const time = timespan || 'all';
+            response = await axios.get(
+              'https://api.5105015032.com/auth/vm3000/top',
+              {
+                params: { subName, time },
+              },
+            );
+
+            break;
+          }
+          default: {
+            filterTag = windowOwner[2];
+            response = await axios.get(
+              'https://api.5105015032.com/auth/vm3000/new',
+              {
+                params: { subName },
+              },
+            );
+            willContinue = true;
+          }
+        }
+
+        break;
+      }
+      case 's': {
+        filterTag = windowOwner[2];
+        response = await axios.get(
+          'https://api.5105015032.com/auth/vm3000/search',
+          {
+            params: { subName },
+          },
+        );
+
+        break;
+      }
+      // No default
     }
     if (filterTag) {
       if (filterTag.toLowerCase() === 'video') {
